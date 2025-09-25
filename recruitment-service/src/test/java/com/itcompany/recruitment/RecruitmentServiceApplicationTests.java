80bc7a3c8e74b38c086ff6440d3c449fe3d45c67
kalabavukasin <149072466+kalabavukasin@users.noreply.github.com>
2025-09-25 18:50:10 +0200

    Project uploaded
    I made mistake, and now i am uploading project

---
+++ recruitment-service/src/test/java/com/itcompany/recruitment/RecruitmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcompany.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
